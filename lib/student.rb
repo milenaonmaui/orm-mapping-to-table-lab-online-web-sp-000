fce26e06467751ebc4354826669e8dd355c292ad
Milena Kari <milenakari@yahoo.com>
2019-01-24 03:42:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
     DB[:conn].execute("DROP TABLE students")
   end
 
-  def create(name:, grade:) 
+  def create(name:, grade:)
     student = Student.new(name,grade)
     student.save
     student
